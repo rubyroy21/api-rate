044433cb0350a1dd5d24b82db724c616bbbe0029
Ruby Roy <rubyroy722@gmail.com>
2022-04-21 01:58:01 +0530

    first commit

---
+++ posts.js
@@ -0,0 +1,25 @@
+ const posts=[
+    {
+
+        "userId":1,
+        "id":1,
+        "title":"Supergirl",
+        "body":"Superhero Series"
+    },
+    {
+
+        "userId":1,
+        "id":2,
+        "title":"Spiderman no way home",
+        "body":"Khichdi bana diya"
+    },
+    {
+
+        "userId":1,
+        "id":3,
+        "title":"Doctor Strange the Multiverse of Madness",
+        "body":"Waiting for this movie eagerly"
+    }
+]
+
+module.exports=posts
